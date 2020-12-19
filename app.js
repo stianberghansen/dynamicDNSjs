b65a2cac3f7e8b2d8862a5abcd1446ca59d79526
stianberghansen <stian@berghansen.me>
2020-12-18 19:55:46 -0500

    Updates startApplication() timeout

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const startApplication = () => {
   let bool = domains
     .startProcess()
     .then((res) => {
-      setTimeout(startApplication, config.timeout * 10);
+      setTimeout(startApplication, config.timeout * 100);
     })
     .catch((err) => {
       console.log(err);
